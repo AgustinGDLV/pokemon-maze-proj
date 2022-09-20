d0972f3aca5f15048d5b28dbc983bafb8921870c
AgustinGDLV <agustingomezdelavega@gmail.com>
2022-09-20 15:22:38 -0700

    added map base and template for caves

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1058,3 +1058,9 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/CaveTemplate/scripts.inc"
+
+	.include "data/maps/CaveBase/scripts.inc"
+
+	.include "data/maps/CaveTemplate_1/scripts.inc"
